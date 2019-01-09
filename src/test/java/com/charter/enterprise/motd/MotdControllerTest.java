2e7d0faf93919ce49558dc7a49f91b8242365ff1
a.treshev <a.treshev@gmail.com>
2019-01-09 10:06:58 -0700

    Changes:  1. Fix the getMotdTest method
    2. Add 2 new tests for the GET and PUT requests which change the message of the day
    
    3. Annotation @DirtiesContext was added to clear context after each test.
       Due to the fact that the  message now stored in a local variable in the controller
       which has the singleton scope, the context should be cleared after each test which change the message.
       Otherwise, the getMotdTest might be failed if it runs after any other.

--- src/test/java/com/charter/enterprise/motd/MotdControllerTest.java
+++ src/test/java/com/charter/enterprise/motd/MotdControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -16,14 +17,39 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+
 public class MotdControllerTest {
     @Autowired
     private MockMvc mvc;
 
     @Test
-    public void getMotd() throws Exception {
+    public void getMotdTest() throws Exception {
+        String initialMessage = "Did I pass the test.";
+        checkMessage(initialMessage);
+    }
+
+    @Test
+    public void changeMessageByGetTest() throws Exception {
+        String newMessage = "updateByGET";
+        mvc.perform(MockMvcRequestBuilders.get("/updateMessage?newMessageOfTheDay=" + newMessage))
+                .andExpect(status().isOk());
+        checkMessage(newMessage);
+    }
+
+    @Test
+    public void changeMessageByPutTest() throws Exception {
+        String newMessage = "updateByPut";
+        mvc.perform(MockMvcRequestBuilders.put("/updateMessage").param("newMessageOfTheDay", newMessage))
+                .andExpect(status().isOk());
+        checkMessage(newMessage);
+    }
+
+    private void checkMessage(String newMessage) throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Goodbye world!")));
+                .andExpect(content().string(equalTo(newMessage)));
     }
+
+
 }
