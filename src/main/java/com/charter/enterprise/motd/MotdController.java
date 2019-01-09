1440802945549307fc798f2a93acd4811f1f23bb
a.treshev <a.treshev@gmail.com>
2019-01-09 10:02:11 -0700

    added 2 methods:
    1) updateMessageFromPutRequest
    recommended method. Just update the message of the day by put request.
    
    2) updateMessageFromGetRequest
    Not recommended, but might be used in case if the user does not have any tools like a curl or telnet
    but have to be able to update message.
    In this case, the message might be updated using the link in any browser
    for example "/updateMessage?newMessageOfTheDay=Hello World!"

--- src/main/java/com/charter/enterprise/motd/MotdController.java
+++ src/main/java/com/charter/enterprise/motd/MotdController.java
@@ -1,16 +1,27 @@
 package com.charter.enterprise.motd;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/")
 @RestController
 public class MotdController {
 
+    private String messageOfTheDay = "Did I pass the test.";
+
     @GetMapping
     public String getMotd() {
-        return "Did I pass the test.";
+        return messageOfTheDay;
     }
 
+    @PutMapping("/updateMessage")
+    public String updateMessageFromPutRequest(@RequestParam String newMessageOfTheDay) {
+        messageOfTheDay = newMessageOfTheDay;
+        return "Message was successfully changed from PUT request to: " + newMessageOfTheDay;
+    }
+
+    @GetMapping("/updateMessage")
+    public String updateMessageFromGetRequest(@RequestParam String newMessageOfTheDay) {
+        messageOfTheDay = newMessageOfTheDay;
+        return "Message was successfully changed from GET request to: " + newMessageOfTheDay;
+    }
 }
